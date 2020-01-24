dd4b80cf902bcf85bb40bdf7e384c6e2dfed06fa
Hidde Beydals <hello@hidde.co>
2020-01-24 02:39:07 +0100

    Unmarshal Docker image labels separately
    To prevent the created field from never getting set when a
    `LabelTimestampFormatError` is returned, which is used as a fallback
    in case the labels have an invalid (non RFC3339 date-time) format.

--- pkg/registry/client.go
+++ pkg/registry/client.go
@@ -134,22 +134,28 @@ interpret:
 			Created time.Time `json:"created"`
 			OS      string    `json:"os"`
 			Arch    string    `json:"architecture"`
+		}
+		if err = json.Unmarshal([]byte(man.History[0].V1Compatibility), &v1); err != nil {
+			return ImageEntry{}, err
+		}
+
+		var config struct {
 			Config  struct {
 				Labels image.Labels `json:"labels"`
 			} `json:"config"`
 		}
-
-		if err = json.Unmarshal([]byte(man.History[0].V1Compatibility), &v1); err != nil {
+		if err = json.Unmarshal([]byte(man.History[0].V1Compatibility), &config); err != nil {
 			if _, ok := err.(*image.LabelTimestampFormatError); !ok {
 				return ImageEntry{}, err
 			}
 			labelErr = err
 		}
+
 		// This is not the ImageID that Docker uses, but assumed to
 		// identify the image as it's the topmost layer.
 		info.ImageID = v1.ID
 		info.CreatedAt = v1.Created
-		info.Labels = v1.Config.Labels
+		info.Labels = config.Config.Labels
 	case *schema2.DeserializedManifest:
 		var man schema2.Manifest = deserialised.Manifest
 		configBytes, err := repository.Blobs(ctx).Get(ctx, man.Config.Digest)
@@ -161,20 +167,27 @@ interpret:
 			Arch            string    `json:"architecture"`
 			Created         time.Time `json:"created"`
 			OS              string    `json:"os"`
+		}
+		if err = json.Unmarshal(configBytes, &config); err != nil {
+			return ImageEntry{}, nil
+		}
+
+		var container struct {
 			ContainerConfig struct {
 				Labels image.Labels `json:"labels"`
 			} `json:"container_config"`
 		}
-		if err = json.Unmarshal(configBytes, &config); err != nil {
+		if err = json.Unmarshal(configBytes, &container); err != nil {
 			if _, ok := err.(*image.LabelTimestampFormatError); !ok {
 				return ImageEntry{}, err
 			}
 			labelErr = err
 		}
+
 		// This _is_ what Docker uses as its Image ID.
 		info.ImageID = man.Config.Digest.String()
 		info.CreatedAt = config.Created
-		info.Labels = config.ContainerConfig.Labels
+		info.Labels = container.ContainerConfig.Labels
 	case *manifestlist.DeserializedManifestList:
 		var list manifestlist.ManifestList = deserialised.ManifestList
 		// TODO(michael): is it valid to just pick the first one that matches?
